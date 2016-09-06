3bcc5a5b0fb72db2847e892825ddf28d5cb24ee5
wimonwan <beam-303@hotmail.com>
2016-09-06 16:31:17 +0700

    Lab3_1 finish

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,51 +1,68 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
     /* your code here */
+    public BigDecimal firstOperand;
+    public BigDecimal secondOperand;
 
     public BinaryCalculator()
     {
         /* your code here */
+        this.firstOperand = new BigDecimal(0);
+        this.secondOperand = new BigDecimal(0);
     }
 
     public void setFirstOperand(Operand operand)
     {
         /* your code here */
+        this.firstOperand = new BigDecimal(operand.operand);
+
     }
 
 
     public void setSecondOperand(Operand operand)
     {
         /* your code here */
+        this.secondOperand = new BigDecimal(operand.operand);
     }
 
     public String add()
     {
         /* your code here */
-        return null;
+        BigDecimal temp = firstOperand.add(secondOperand);
+        temp = temp.stripTrailingZeros(); //จุดหาย
+        return temp.toString();
     }
 
     public String subtract()
     {
         /* your code here */
-        return null;
+        BigDecimal temp = firstOperand.subtract(secondOperand);
+        temp = temp.stripTrailingZeros();
+        return temp.toString();
     }
 
     public String multiply()
     {
         /* your code here */
-        return null;
+        BigDecimal temp = firstOperand.multiply(secondOperand);
+        temp = temp.stripTrailingZeros();
+        return temp.toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
         /* your code here */
-        return null;
+        BigDecimal temp = firstOperand.divide(secondOperand,5,3);
+        temp = temp.stripTrailingZeros();
+        return temp.toString();
     }
 
 
