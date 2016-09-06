28a9a6c0bc89004431b1b83fe28d092970740547
Pimukthee\wobwab <pimukthee@gmail.com>
2016-09-06 15:12:24 +0700

    580610662 LAB3 done

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -3,49 +3,61 @@ package cpe200;
 /**
  * Created by pruet on 5/9/2559.
  */
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BinaryCalculator {
     /* your code here */
-
+    private Operand first;
+    private Operand second;
     public BinaryCalculator()
     {
-        /* your code here */
+        //this.first.operand = "";
+       // this.second.operand = "";
     }
 
     public void setFirstOperand(Operand operand)
     {
-        /* your code here */
+        this.first = operand;
     }
 
 
     public void setSecondOperand(Operand operand)
     {
-        /* your code here */
+        this.second = operand;
     }
 
     public String add()
     {
-        /* your code here */
-        return null;
+        BigDecimal temp1 = new BigDecimal(this.first.operand);
+        BigDecimal temp2 =temp1.add(new BigDecimal(this.second.operand)).stripTrailingZeros();
+
+        return temp2.toString();
     }
 
     public String subtract()
     {
-        /* your code here */
-        return null;
+        BigDecimal temp1 = new BigDecimal(this.first.operand);
+        BigDecimal temp2 =temp1.subtract(new BigDecimal(this.second.operand)).stripTrailingZeros();
+        return temp2.toString();
     }
 
     public String multiply()
     {
-        /* your code here */
-        return null;
+        BigDecimal temp1 = new BigDecimal(this.first.operand);
+        BigDecimal temp2 =  temp1.multiply(new BigDecimal(this.second.operand)).stripTrailingZeros();
+        return temp2.toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
-        /* your code here */
-        return null;
+        BigDecimal temp1 = new BigDecimal(this.first.operand);
+        BigDecimal temp2 = new BigDecimal(this.second.operand);
+        if(temp2.compareTo(BigDecimal.ZERO)==0)
+        throw new IllegalArgumentException("Argument 'divisor' is 0");
+        temp2 = temp1.divide(temp2,5, RoundingMode.HALF_UP).stripTrailingZeros();
+        return temp2.toString();
     }
 
 
