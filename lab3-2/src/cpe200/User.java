97abf46552173d25d032cd35d02b19d571f90252
Eakkaphop Wongsorn <eakkaphop.w@gmail.com>
2016-09-06 17:38:32 +0700

    Update User.java

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -1,10 +1,8 @@
-package cpe200;
-
 /**
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    protected String userName;
+    public String userName;
     protected String password;
 
     public User()
@@ -14,25 +12,33 @@ public class User {
 
     public boolean setUserName(String name)
     {
-        /* your code here */
+        String regex = "^[A-Za-z][A-Za-z0-9]{7,}$";
+        if(name.matches(regex)){
+                   userName = name;
+                   return true;
+              }
         return false;
     }
 
-    public boolean setPassword(String name)
+    public boolean setPassword(String pass)
     {
-        /* your code here */
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{12,}$";
+        if(pass.matches(regex)){
+            password = pass;
+            return true;
+        }
         return false;
     }
 
     public String getUserName()
     {
         /* your code here */
-        return null;
+        return userName;
     }
 
     public String getPassword()
     {
         /* your code here */
-        return null;
+        return password;
     }
 }
