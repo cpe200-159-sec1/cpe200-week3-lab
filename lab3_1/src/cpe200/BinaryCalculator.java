6ab12f894e08880947829994918c5e87a9529203
Eakkaphop Wongsorn <eakkaphop.w@gmail.com>
2016-09-06 17:38:13 +0700

    Update BinaryCalculator.java

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -3,49 +3,60 @@ package cpe200;
 /**
  * Created by pruet on 5/9/2559.
  */
+import java.math.BigDecimal;
 
 public class BinaryCalculator {
-    /* your code here */
+    public BigDecimal firstOperand;
+    public BigDecimal secondOperand;
 
     public BinaryCalculator()
     {
-        /* your code here */
+        firstOperand = new BigDecimal(0.0);
+        secondOperand = new BigDecimal(0.0);
     }
 
     public void setFirstOperand(Operand operand)
     {
-        /* your code here */
+        firstOperand = new BigDecimal(operand.operand);
     }
 
 
     public void setSecondOperand(Operand operand)
     {
-        /* your code here */
+        secondOperand = new BigDecimal(operand.operand);
     }
 
     public String add()
     {
         /* your code here */
-        return null;
+        firstOperand = firstOperand.add(secondOperand);
+        BigDecimal bigDecimal = firstOperand.stripTrailingZeros();
+        return bigDecimal.toString();
     }
 
     public String subtract()
     {
         /* your code here */
-        return null;
+        firstOperand = firstOperand.subtract(secondOperand);
+        BigDecimal bigDecimal = firstOperand.stripTrailingZeros();
+        return bigDecimal.toString();
     }
 
     public String multiply()
     {
         /* your code here */
-        return null;
+        firstOperand = firstOperand.multiply(secondOperand);
+        BigDecimal bigDecimal = firstOperand.stripTrailingZeros();
+        return bigDecimal.toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
         /* your code here */
-        return null;
+        firstOperand = firstOperand.divide(secondOperand,5,BigDecimal.ROUND_HALF_UP);
+        BigDecimal bigDecimal = firstOperand.stripTrailingZeros();
+        return bigDecimal.toString();
     }
 
 
