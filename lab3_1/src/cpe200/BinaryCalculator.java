dfc75ec6ab60cf5c32171eeb2c03fbd36547ad8f
Wiphob <wiphobkaoian@gmail.com>
2016-09-06 18:19:54 +0700

    580610677  Finished Lab3-1 BinaryCalculator

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,52 +1,55 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
-    /* your code here */
+    private  BigDecimal FirstOP;
+    private  BigDecimal SecondOP;
+
 
     public BinaryCalculator()
     {
-        /* your code here */
+        FirstOP = new BigDecimal(0);
+        SecondOP = new BigDecimal(0);
     }
 
     public void setFirstOperand(Operand operand)
     {
-        /* your code here */
+        FirstOP = new BigDecimal(operand.operand);
     }
 
 
     public void setSecondOperand(Operand operand)
     {
-        /* your code here */
+        SecondOP = new BigDecimal(operand.operand);
     }
 
     public String add()
     {
-        /* your code here */
-        return null;
+        return FirstOP.add(SecondOP).stripTrailingZeros().toString();
     }
 
     public String subtract()
     {
-        /* your code here */
-        return null;
+        return FirstOP.subtract(SecondOP).stripTrailingZeros().toString();
     }
 
     public String multiply()
     {
-        /* your code here */
-        return null;
+
+        return FirstOP.multiply(SecondOP).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
-        /* your code here */
-        return null;
+        if(SecondOP.equals(0)) {
+            throw new ArithmeticException("ERROR");
+        }
+        return FirstOP.divide(SecondOP,5,BigDecimal.ROUND_UP).stripTrailingZeros().toString();
     }
-
-
 }
