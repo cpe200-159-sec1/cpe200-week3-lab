c53a32726a3c961fd9a5122dbb2fb069ad42e56d
oraya1996 <jjanemark2345@gmail.com>
2016-09-06 15:52:08 +0700

    580610687

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,52 +1,52 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
-    /* your code here */
+    private BigDecimal firstopereand;
+    private BigDecimal secondoperand;
 
-    public BinaryCalculator()
-    {
-        /* your code here */
+    public BinaryCalculator() {
+        firstopereand = new BigDecimal(0);
+        secondoperand = new BigDecimal(0);
+
     }
 
-    public void setFirstOperand(Operand operand)
-    {
-        /* your code here */
+    public void setFirstOperand(Operand operand) {
+        firstopereand = new BigDecimal(operand.operand);
     }
 
 
-    public void setSecondOperand(Operand operand)
-    {
-        /* your code here */
+    public void setSecondOperand(Operand operand) {
+        secondoperand = new BigDecimal(operand.operand);
     }
 
-    public String add()
-    {
-        /* your code here */
-        return null;
+    public String add() {
+        return firstopereand.add(secondoperand).stripTrailingZeros().toString();
+
     }
 
-    public String subtract()
-    {
-        /* your code here */
-        return null;
+    public String subtract() {
+        return firstopereand.subtract(secondoperand).stripTrailingZeros().toString();
     }
 
-    public String multiply()
-    {
-        /* your code here */
-        return null;
+    public String multiply() {
+        return firstopereand.multiply(secondoperand).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division()
-    {
-        /* your code here */
-        return null;
+    public String division() {
+        if (secondoperand.compareTo(BigDecimal.ZERO) == 0)
+            throw new IllegalArgumentException("Argument 'divisor' is 0");
+        secondoperand = firstopereand.divide(secondoperand, 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+
+        return secondoperand.toString();
     }
-
-
 }
+
+
+
