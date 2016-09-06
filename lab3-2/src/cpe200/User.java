348cec452738eadc16f9bd355129bdefee9a25d8
Chawasit Tengtrairatana <chawasit_teng@cmu.ac.th>
2016-09-06 13:30:23 +0700

    580610631 lab3_2

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -1,5 +1,6 @@
 package cpe200;
 
+
 /**
  * Created by pruet on 6/9/2559.
  */
@@ -9,30 +10,36 @@ public class User {
 
     public User()
     {
-        /* your code here */
+
     }
 
     public boolean setUserName(String name)
     {
-        /* your code here */
+        String regex = "^[A-Za-z][A-Za-z0-9]{7,}$";
+        if(name.matches(regex)) {
+            this.userName = name;
+            return true;
+        }
         return false;
     }
 
     public boolean setPassword(String name)
     {
-        /* your code here */
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]{12,})$";
+        if(name.matches(regex)) {
+            this.password = name;
+            return true;
+        }
         return false;
     }
 
     public String getUserName()
     {
-        /* your code here */
-        return null;
+        return userName;
     }
 
     public String getPassword()
     {
-        /* your code here */
-        return null;
+        return password;
     }
 }
