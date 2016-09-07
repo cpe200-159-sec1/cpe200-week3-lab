d6d815375be5449b5143afc02c3c8f9b50fddba6
KaninSukarote <kanin.sukarote@gmail.com>
2016-09-07 19:12:19 +0700

    580610622

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -10,29 +10,41 @@ public class User {
     public User()
     {
         /* your code here */
+
     }
 
     public boolean setUserName(String name)
     {
         /* your code here */
+        String regex = "^[A-Za-z][A-Za-z0-9]{7,}$";
+        if(name.matches(regex)) {
+            this.userName = name;
+            return true;
+        }
+
         return false;
     }
 
     public boolean setPassword(String name)
     {
         /* your code here */
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]{12,})$";
+        if(name.matches(regex)) {
+            this.password = name;
+            return true;
+        }
         return false;
     }
 
     public String getUserName()
     {
         /* your code here */
-        return null;
+        return userName;
     }
 
     public String getPassword()
     {
         /* your code here */
-        return null;
+        return password;
     }
 }
--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,51 +1,59 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
-    /* your code here */
+    private BigDecimal myOrandfist;
+    private BigDecimal myOrandsecond;
 
     public BinaryCalculator()
     {
         /* your code here */
+        myOrandfist = new BigDecimal(0);
+        myOrandsecond = new BigDecimal(0);
+
     }
 
     public void setFirstOperand(Operand operand)
     {
         /* your code here */
+        myOrandfist = new BigDecimal(operand.operand);
     }
 
 
     public void setSecondOperand(Operand operand)
     {
         /* your code here */
+        myOrandsecond = new BigDecimal(operand.operand);
     }
 
     public String add()
     {
         /* your code here */
-        return null;
+        return myOrandfist.add(myOrandsecond).stripTrailingZeros().toString();
     }
 
     public String subtract()
     {
         /* your code here */
-        return null;
+        return myOrandfist.subtract(myOrandsecond).stripTrailingZeros().toString();
     }
 
     public String multiply()
     {
         /* your code here */
-        return null;
+        return myOrandfist.multiply(myOrandsecond).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
         /* your code here */
-        return null;
+        return myOrandfist.divide(myOrandsecond,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
     }
 
 
