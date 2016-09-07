32356f21aa40e7a445c0e4b823184a7e49daeb55
ather <kandanich@hotmail.com>
2016-09-07 15:39:08 +0700

    580610688 Fin Lab

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -7,32 +7,42 @@ public class User {
     protected String userName;
     protected String password;
 
+
+
     public User()
     {
         /* your code here */
+        userName = "";
+        password = "";
     }
 
     public boolean setUserName(String name)
     {
         /* your code here */
-        return false;
+        if(name.matches("^[a-zA-Z][a-zA-Z0-9]{7,}$")){
+            userName = name;
+            return true;
+        }return false;
     }
 
     public boolean setPassword(String name)
     {
         /* your code here */
-        return false;
+        if(name.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{12,}$")){
+            password = name;
+            return true;
+        }return false;
     }
 
     public String getUserName()
     {
         /* your code here */
-        return null;
+        return userName;
     }
 
     public String getPassword()
     {
         /* your code here */
-        return null;
+        return password;
     }
 }
--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,51 +1,62 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
     /* your code here */
+    private BigDecimal x;
+    private BigDecimal y;
 
     public BinaryCalculator()
     {
         /* your code here */
+        x = new BigDecimal(0);
+        y = new BigDecimal(0);
     }
 
     public void setFirstOperand(Operand operand)
     {
         /* your code here */
+        x = new BigDecimal(operand.operand);
+
     }
 
 
     public void setSecondOperand(Operand operand)
     {
         /* your code here */
+        y = new BigDecimal(operand.operand);
     }
 
     public String add()
     {
         /* your code here */
-        return null;
+        return x.add(y).stripTrailingZeros().toString();
     }
 
     public String subtract()
     {
         /* your code here */
-        return null;
+        return x.subtract(y).stripTrailingZeros().toString();
     }
 
     public String multiply()
     {
         /* your code here */
-        return null;
+        return x.multiply(y).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
         /* your code here */
-        return null;
+        if(y.equals(0))throw new ArithmeticException("Not Divided by zero");
+        else
+            return x.divide(y,5,BigDecimal.ROUND_UP).stripTrailingZeros().toString();
     }
 
 
